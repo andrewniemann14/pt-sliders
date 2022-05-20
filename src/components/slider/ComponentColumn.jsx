13aea36b50f25342e448b61038c9657d336cef29
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-20 16:39:38 -0500

    changed heading

--- src/components/slider/ComponentColumn.jsx
+++ src/components/slider/ComponentColumn.jsx
@@ -6,7 +6,21 @@ import Slider from './Slider'
 
 export default function ComponentColumn({category, data, handlePoints}) {
   const components = Object.keys(data);
-  const capitalizedCat = category.slice(0,1).toUpperCase()+category.slice(1);
+  // const capitalizedCat = category.slice(0,1).toUpperCase()+category.slice(1);
+  let categoryName;
+  switch (category) {
+    case 'strength':
+      categoryName = 'STR';
+      break;
+    case 'endurance':
+      categoryName = 'END';
+      break;
+    case 'cardio':
+      categoryName = 'VO2';
+      break;
+    default:
+      break;
+  }
 
   const [component, setComponent] = useState(components[0])
   
@@ -15,8 +29,8 @@ export default function ComponentColumn({category, data, handlePoints}) {
   }
 
   return (
-    <div className="flex flex-col text-center">
-      <h3>{capitalizedCat}</h3>
+    <div className="flex flex-col text-center w-1/4 h-80">
+      <h3>{categoryName}</h3>
       <ComponentSelector handleSelect={handleSelect} options={components} />
 
       <Slider data={data[component]} handlePoints={handlePoints} />
