7554d13483a399429537a37f7980e2f02fb0496a
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-25 17:14:44 -0500

    ready for further changes

--- src/components/slider/ComponentSelector.jsx
+++ src/components/slider/ComponentSelector.jsx
@@ -1,12 +1,13 @@
 
 
-export default function ComponentSelector({ handleSelect, options }) {
+export default function ComponentSelector({ data, handleSelect, options }) {
 
   return (
     <select onChange={e => handleSelect(e.target.value)}>
       {options.map(o => {
         return (
-          <option key={o} value="" className="">{o}</option>
+          // TODO: make a modal that displays full component name: data[o].component
+          <option key={o} value={o} className="">{o}</option>
         )
       })}
     </select>
