f2b3434fc7e6dbcc75d1565d947100232c9ca7ad
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-25 17:14:00 -0500

    adjusted for reversed JSON

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,21 +3,22 @@ import "../../css/slider.css";
 
 // TODO: definitely need to get TypeScript in here
 export default function Slider({ data, handlePointsBottom }) {
-  const repsArray = Array(data.reps)[0];
-  const pointsArray = Array(data.points)[0];
+  const repsArray = Array(data.reps)[0]
+  const pointsArray = Array(data.points)[0]
   const max = repsArray.length;
 
-  const [sliderPos, setSliderPos] = useState(0);
+  const [sliderPos, setSliderPos] = useState(-max+1);
   const [reps, setReps] = useState(repsArray[0]);
   const [points, setPoints] = useState(pointsArray[0]);
   const [labelHeight, setLabelHeight] = useState(0 / max);
 
 
+  // Math.abs being used to handle reverse order of reps/points in JSON
   useEffect(() => {
-    setReps(repsArray[sliderPos]);
-    setPoints(pointsArray[sliderPos]);
-    setLabelHeight((sliderPos / max) * 100);
-  }, [sliderPos]);
+    setReps(repsArray[Math.abs(sliderPos)]);
+    setPoints(pointsArray[Math.abs(sliderPos)]);
+    setLabelHeight((Math.abs(sliderPos) / max) * 100 - 5);
+  }, [data, sliderPos]);
 
   // never refer to state in the same useEffect in which it's updated
   // this is why my points were using the PREVIOUS slider index
@@ -36,8 +37,9 @@ export default function Slider({ data, handlePointsBottom }) {
         <input
           type="range"
           name="slider"
-          min={0}
-          max={max - 1}
+          // min and max switched to handle reversed JSON
+          max={0}
+          min={-max + 1}
           value={sliderPos}
           className="cursor-pointer slider w-full h-full"
           onChange={e => setSliderPos(e.target.value)}
@@ -45,15 +47,11 @@ export default function Slider({ data, handlePointsBottom }) {
         />
 
         <p className="absolute w-full h-full top-0 left-0 pointer-events-none">
-          <span className="absolute px-1 left-0 text-left" style={{ bottom: labelHeight + "%" }}>
+          {/* top being used instead of bottom to handle reversed JSON */}
+          <span className="absolute px-1 left-0 text-left font-bold" style={{ top: labelHeight + "%" }}>
             {reps}
           </span>
         </p>
-      </div>
-
-      <div className="flex justify-between px-1">
-        <span></span>
-        <span>Pts</span>
       </div>
 
     </div>
