fc97b25f23cd273ca35719669bd59acc02911220
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-25 17:14:29 -0500

    color, spacing

--- src/components/OverallScore.jsx
+++ src/components/OverallScore.jsx
@@ -4,13 +4,12 @@ export default function OverallScore({score}) {
   // let scoreNum = Number(score);
   let color;
   if (score < 75) color = 'text-red-700';
-  else if (score < 80) color = 'text-lime-500';
-  // else if (score < 90) color = 'text-green-700';
-  else color = 'text-green-700';
-
+  else if (score < 80) color = 'text-lime-600';
+  else if (score < 90) color = 'text-green-600';
+  else color = 'text-teal-600';
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center py-2">
       {score >= 90 && <Star />}
       <h1 className={'font-bold text-center mx-4 text-4xl ' + color}>{score}</h1>
       {score >= 90 && <Star />}
