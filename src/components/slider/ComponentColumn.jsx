4e636d35a868900458f841352245162077f95540
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-25 17:09:50 -0500

    add category logos

--- src/components/slider/ComponentColumn.jsx
+++ src/components/slider/ComponentColumn.jsx
@@ -2,44 +2,34 @@ import { useState } from 'react'
 
 import ComponentSelector from './ComponentSelector'
 import Slider from './Slider'
+import { ReactComponent as Pushups } from '../../img/pushups-man-svgrepo-com.svg';
+import { ReactComponent as Situps } from '../../img/vending-knees-svgrepo-com.svg';
+import { ReactComponent as Running } from '../../img/running-man-svgrepo-com.svg';
 
 
 export default function ComponentColumn({category, data, handlePointsTop}) {
-  const components = Object.keys(data);
-  // const capitalizedCat = category.slice(0,1).toUpperCase()+category.slice(1);
-  const [compScore, setCompScore] = useState();
-  let categoryName;
-  switch (category) {
-    case 'strength':
-      categoryName = 'STR';
-      break;
-    case 'endurance':
-      categoryName = 'END';
-      break;
-    case 'cardio':
-      categoryName = 'VO2';
-      break;
-    default:
-      break;
-  }
-
+  const components = Object.keys(data); // TODO:
   const [component, setComponent] = useState(components[0])
-  
   function handleSelect(option) {
     setComponent(option);
   }
-
-  function handlePointsMiddle(points) {
-    setCompScore(points);
-    handlePointsTop(points)
-  }
+  
+    const [compScore, setCompScore] = useState();
+    function handlePointsMiddle(points) {
+      setCompScore(points);
+      handlePointsTop(points)
+    }
+  
 
   return (
     <div className="flex flex-col text-center w-1/4 h-full">
-      <h3 className='flex justify-between'><span>{categoryName}:</span><span className='font-bold'>{compScore}</span></h3>
+      <div className='flex justify-between mb-2'>
+        {category === 'strength' ? <Pushups /> : category === 'endurance' ? <Situps /> : <Running />}
+        <span className='font-bold'>{compScore}</span>
+      </div>
 
       <Slider data={data[component]} handlePointsBottom={handlePointsMiddle} />
-      <ComponentSelector handleSelect={handleSelect} options={components} />
+      <ComponentSelector handleSelect={handleSelect} options={components} data={data} />
     </div>
   );
 }
