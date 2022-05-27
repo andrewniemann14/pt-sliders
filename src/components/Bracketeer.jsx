9fcc5ca3d87dc312d247abc189d5c4fe7c498eed
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-27 07:19:03 -0500

    fixed mistake

--- src/components/Bracketeer.jsx
+++ src/components/Bracketeer.jsx
@@ -11,7 +11,7 @@ export default function Bracketeer({ handleBracket }) {
 
 
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center mt-4 space-x-1">
       <select name="" id="" onChange={e => setSex(e.target.value)}>
         <option value="male" selected>Male</option>
         <option value="female">Female</option>
@@ -20,7 +20,7 @@ export default function Bracketeer({ handleBracket }) {
         <option value="17-24">17-24</option>
         <option value="25-29">25-29</option>
         <option value="30-34" selected>30-34</option>
-        <option value="35-39">25-29</option>
+        <option value="35-39">35-39</option>
         <option value="40-44">40-44</option>
       </select>
 
