e3b91c20552a34cab2c0b700993df67fc7f05c3d
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-05-28 20:00:47 -0500

    word change

--- src/components/menu/MenuPane.jsx
+++ src/components/menu/MenuPane.jsx
@@ -4,7 +4,7 @@ export default function MenuPane({ open }) {
   return (
     <div className={`flex flex-col right-0 top-0 h-full w-64 absolute z-10 transition-all bg-neutral-800 text-white ${open ? "" : "translate-x-[100%] bg-transparent"}`}>
       {/* logo here */}
-      <a href="https://www.youtube.com/watch?v=8mPicXen9nA" className="text-center mt-12 underline">Shuttle run video</a>
+      <a href="https://www.youtube.com/watch?v=8mPicXen9nA" className="text-center mt-12 underline">Shuttle run practice</a>
       {/* make this a block, welcoming feedback */}
       <a href="mailto:pt@niemann.app" className="text-center m-4 underline">Contact me</a>
 
